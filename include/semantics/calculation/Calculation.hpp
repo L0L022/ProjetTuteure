69acbcbf0719bab3eb616d9bc4bd1a2fe0891ea8
LinkIsACake <leo.sudreau.sin@gmail.com>
2018-02-20 15:22:24 +0100

    calcul start

---
+++ include/semantics/calculation/Calculation.hpp
@@ -0,0 +1,15 @@
+#ifndef SEMANTICS_CALCULATION_HPP
+#define SEMANTICS_CALCULATION_HPP
+
+
+struct question {
+    int nb_pers;
+    
+
+};
+
+class Calculation {
+    static SimpleGraph();
+};
+
+#endif
