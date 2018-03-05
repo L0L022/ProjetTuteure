26c0c4f3f7bc59f586c497da7f86e81e4118e34d
Travis CI <travis@travis-ci.org>
2018-03-05 21:20:27 +0000

    Deploy code docs to GitHub Pages Travis build: 9
    Commit: 91f5eab690b7cdacf27178e4d14c192f5b9087f2

---
+++ html/navtreedata.js
@@ -0,0 +1,12 @@
+var NAVTREE =
+[
+  [ "ProjetTuteure", "index.html", ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'cliquez pour désactiver la synchronisation du panel';
+var SYNCOFFMSG = 'cliquez pour activer la synchronisation du panel';
