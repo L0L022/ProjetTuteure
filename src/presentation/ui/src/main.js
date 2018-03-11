cb79237962d80128ade266e7f2d05105e2818035
kaddou69200 <33481461+kaddou69200@users.noreply.github.com>
2018-03-11 18:26:13 +0100

    1

--- src/presentation/ui/src/main.js
+++ src/presentation/ui/src/main.js
@@ -1,6 +1,3 @@
-@@ -1,24 +0,0 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
