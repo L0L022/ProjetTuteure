556d5dece6b7d51ac12ff7983cba2c3a0cffd75d
= <leo@leo-pc>
2018-03-22 13:39:18 +0100

    graph simple fonctionnel

--- include/semantics/calculation/SimpleGraph.hpp
+++ include/semantics/calculation/SimpleGraph.hpp
@@ -2,10 +2,9 @@
 #define SEMANTICS_SIMPLEGRAPH_HPP
 
 #include <unordered_map>
-#include <pair>
+#include <utility>
 #include <vector>
 #include <string>
-#include <tuple>
 
 namespace semantics {
 namespace core {
@@ -23,22 +22,21 @@ namespace core {
     int sum;
 
     middleResult(int _occurences, int _sum)
-    :_occurences(occurences),_sum(sum){};
+    :occurences(_occurences),sum(_sum){};
   };
 
   struct result {
-    std::string _word;
+    std::string word;
     float middleRank;
     float frequency;
 
-    result(std::strind _word, float _middleRank, float _frequency)
+    result(std::string _word, float _middleRank, float _frequency)
     :word(_word),middleRank(_middleRank),frequency(_frequency){};
   };
 
-  typedef std::vector<std::string> stringVec
+  typedef std::vector<std::string> stringVec;
   typedef std::unordered_map<std::string,middleResult> unordered_map;
   typedef std::vector<result> results;
-
 
   results Calcul(std::vector<stringVec> form);
 
